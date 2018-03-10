e9782b4603bcbe68e9ac2f9145df16c8b84f9528
Briton Powe <britonpowe1@Britons-MBP.attlocal.net>
2018-03-10 15:11:03 -0600

    Add game board functions for: Valid sums, and game over

--- p2tictactoe.py
+++ p2tictactoe.py
@@ -1,11 +1,11 @@
 import random
 
-gameBoard = [1,1,1,1,
+gameBoard = [1,1,1,0,
              2,3,3,3,
              2,4,5,5,
              2,4,6,7]
 
-winSpaces = [[0,1,2,3],
+winSpaceIndex = [[0,1,2,3],
              [4,5,6,7],
              [8,9,10,11],
              [12,13,14,15],
@@ -16,19 +16,47 @@ winSpaces = [[0,1,2,3],
              [0,5,10,15],
              [3,6,9,12]]
 
-currentSums = [0,0,0,0,0,0,0,0,0,0]
+winSpaceValues = []
+currentSums = []
+END_SUM = 34
+inEndState = False
 
-def calculateSum():
-    total = 0
-    currentSpace = []
-    currentSumsIndex = 0
-    for element in winSpaces:
+
+def calculateSpaceValues():
+    currentSpaceGroup = []
+    for element in winSpaceIndex:
         for index in element:
-            currentSpace.append(gameBoard[index])
-        print(currentSpace)
-        currentSums[currentSumsIndex] = sum(currentSpace)
-        print(currentSums)
-        currentSumsIndex += 1
+            currentSpaceGroup.append(gameBoard[index])
+        winSpaceValues.append(currentSpaceGroup)
+        currentSpaceGroup = []
+  
 
-calculateSum()
-#def checkTerminalState()
+calculateSpaceValues()
+
+
+def calculateSums():
+    for index in range(len(winSpaceValues)):
+        currentSums.append(sum(winSpaceValues[index]))
+    print(currentSums)
+
+
+calculateSums()
+
+
+def determineEndState(inEndState):
+    if not gameBoard.__contains__(0):
+        inEndState = True
+    else:
+        for sumIndex, sumValue in enumerate(currentSums, 0):
+            if sumValue == END_SUM:
+                if not winSpaceValues[sumIndex].__contains__(0):
+                    inEndState = True
+    print(inEndState)
+
+determineEndState(inEndState)
+
+def insertValue(value, index):
+    gameBoard[index] = value
+
+def reevaluateBoard():
+    
