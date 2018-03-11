2863395271c57f1b683de7c9bb901160ce84197e
Briton Powe <britonpowe1@Britons-MBP.attlocal.net>
2018-03-11 16:32:45 -0500

    created main game file and did tests

--- Player.py
+++ Player.py
@@ -59,46 +59,46 @@ class Player:
             except ValueError:
                 print("\nError! Please only enter an integer for value and index.")
 
-board = Board.Board()
-maxS = True
-player1 = Player(maxS)
-maxS2 = False
-player2 = Player(maxS2)
+# board = Board.Board()
+# maxS = True
+# player1 = Player(maxS)
+# maxS2 = False
+# player2 = Player(maxS2)
 
 
-turn = 0
-previousTurn = 1
-while board.isTerminalState() != True:
-    if turn == 0:
-        board.printBoard()
-        print("Turn - Player 1")
-        print("Available Values: ", player1.getAvailibleValues())
-        player1.setAvailableSpaces(board)
-        print("Available Spaces: ", player1.getAvailableSpaces())
-        player1.makeMove(board)
-        turn = 1
-        previousTurn = 0
-        os.system('cls' if os.name == 'nt' else 'clear')
-    else:
-        board.printBoard()
-        print("Turn - Player 2")
-        print("Available Values: ", player2.getAvailibleValues())
-        player2.setAvailableSpaces(board)
-        print("Available Spaces: ", player2.getAvailableSpaces())
-        player2.makeMove(board)
-        turn = 0
-        previousTurn = 1
-        os.system('cls' if os.name == 'nt' else 'clear')
+# turn = 0
+# previousTurn = 1
+# while board.isTerminalState() != True:
+#     if turn == 0:
+#         board.printBoard()
+#         print("Turn - Player 1")
+#         print("Available Values: ", player1.getAvailibleValues())
+#         player1.setAvailableSpaces(board)
+#         print("Available Spaces: ", player1.getAvailableSpaces())
+#         player1.makeMove(board)
+#         turn = 1
+#         previousTurn = 0
+#         os.system('cls' if os.name == 'nt' else 'clear')
+#     else:
+#         board.printBoard()
+#         print("Turn - Player 2")
+#         print("Available Values: ", player2.getAvailibleValues())
+#         player2.setAvailableSpaces(board)
+#         print("Available Spaces: ", player2.getAvailableSpaces())
+#         player2.makeMove(board)
+#         turn = 0
+#         previousTurn = 1
+#         os.system('cls' if os.name == 'nt' else 'clear')
 
-board.printBoard()
-print("End of game")
-if board.printWinningSpace() != []:
-    if previousTurn == 0:
-        print("Player 1 Wins!\nWinning Space:", board.printWinningSpace())
-    else:
-        print("Player 2 Wins!\nWinning Space:", board.printWinningSpace())
-else:
-    print("Tie Game!")
+# board.printBoard()
+# print("End of game")
+# if board.printWinningSpace() != []:
+#     if previousTurn == 0:
+#         print("Player 1 Wins!\nWinning Space:", board.printWinningSpace())
+#     else:
+#         print("Player 2 Wins!\nWinning Space:", board.printWinningSpace())
+# else:
+#     print("Tie Game!")
 
-print("\n\n\n")
-del player1, player2, board
+# print("\n\n\n")
+# del player1, player2, board
