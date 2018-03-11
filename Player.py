0e4b569270e3624ece6bf4b517e65cec143dfb43
Briton Powe <britonpowe1@Britons-MBP.attlocal.net>
2018-03-11 13:43:52 -0500

    Testing player functions

--- Player.py
+++ Player.py
@@ -1,12 +1,73 @@
-from .Board import Board
+import Board
 
 class Player:
     def __init__(self, maxStatus):
         self.__availableValues = []
         self.__isMax = maxStatus
         self.__availableSpaces = []
+        self.__currentState = []
+        self.__setAvailableValues()
 
     def setAvailableSpaces(self, board):
-        self.__availableSpaces = board.__availableSpaces
+        self.__availableSpaces = board.availableMoves()
 
+    def getAvailableSpaces(self):
+        return self.__availableSpaces
 
+    def setState(self, board):
+        self.__currentState = board.getBoardState()
+
+    def __setAvailableValues(self):
+        if self.__isMax == True:
+            self.__availableValues = [1,3,5,7,9,11,13,15]
+        else:
+            self.__availableValues = [2,4,6,8,10,12,14,16]
+
+    def getAvailibleValues(self):
+        return self.__availableValues
+
+    def makeMove(self, board):
+        value = index = None
+        #Input validation for value and index
+        self.setAvailableSpaces(board)
+        while value is None:
+            
+            try:
+                chosenValue = input("\nEnter a value: \n\n")
+                value = int(chosenValue)
+                if not self.__availableValues.__contains__(value):
+                    print("\nError! Please enter the available values: ", self.__availableValues)
+                    value = None
+                else:
+
+                     while index is None:
+                         try:
+                             chosenIndex = input("\nEnter the index(0-15): \n\n")
+                             index = int(chosenIndex)
+                             if not self.__availableSpaces.__contains__(index):
+                                 print("\nError! Please eneter the availible indexes: ", self.__availableSpaces)
+                                 index = None
+                             else:
+                                board.setValue(value, index)
+                                self.setState(board)
+                                self.setAvailableSpaces
+                                self.__availableValues.remove(value)
+                         except ValueError:
+                            print("\nError! Please only enter an integer for value and index.")   
+            
+            except ValueError:
+                print("\nError! Please only enter an integer for value and index.")
+
+board = Board.Board()
+board.printBoard()
+maxS = True
+player1 = Player(maxS)
+maxS2 = False
+player2 = Player(maxS2)
+
+print("Turn - Player 1")
+print("Available Values: ", player1.getAvailibleValues())
+player1.setAvailableSpaces(board)
+print("Available Spaces: ", player1.getAvailableSpaces())
+
+del player1, player2, board
