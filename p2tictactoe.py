639e5293afda21383700333a02cc9961b5ead175
Briton Powe <britonpowe1@Britons-MBP.attlocal.net>
2018-03-10 20:27:24 -0600

    creating board class

--- p2tictactoe.py
+++ p2tictactoe.py
@@ -1,5 +1,59 @@
 import random
 
+class Board:
+    def __init__(self):
+        self.__endSUM = 34
+        self.__isTerminal = False
+        self.__currentSums = []
+        self.__winSpaceValues = []
+        self.__gameBoard = [0,0,0,0,
+                            0,0,0,0,
+                            0,0,0,0,
+                            0,0,0,0]
+        self.__winSpaceIndex = [[0,1,2,3],
+                                [4,5,6,7],
+                                [8,9,10,11],
+                                [12,13,14,15],
+                                [0,4,8,12],
+                                [1,5,9,13],
+                                [2,6,10,14],
+                                [3,7,11,15],
+                                [0,5,10,15],
+                                [3,6,9,12]]
+    
+    def getBoardState(self):
+        return self.__gameBoard
+
+    def resetBoard(self):
+        for index in self.__gameBoard:
+            self.__gameBoard[index] = 0
+        self.__isTerminal = False
+        self.__winSpaceValues[:] = []
+        self.__currentSums[:] = []
+    
+    def __setWinSpaceValues(self):
+        self.__winSpaceValues[:]=[]  
+        currentSpaceGroup = []
+        for element in winSpaceIndex:
+            for index in element:
+                currentSpaceGroup.append(self.__gameBoard[index])
+            self.__winSpaceValues.append(currentSpaceGroup)
+            currentSpaceGroup = []
+
+    def __setSums(self):
+        self.__currentSums[:] = []
+        for index in range(len(self.__winSpaceValues)):
+            self.__currentSums.append(sum(self.__winSpaceValues[index]))
+
+    def recalculate(self):
+        self.__setWinSpaceValues
+
+
+    def setValue(self, value, index):
+        self.__gameBoard[index] = value
+
+
+
 gameBoard = [0,0,10,10,
              0,0,0,0,
              0,0,0,0,
@@ -71,7 +125,6 @@ def insertValue(value, index, board):
 def resetBoard(board, state):
     for index in board:
         board[index] = 0
-    reevaluateBoard
     state = False
 
 
